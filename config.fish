b42c6f72a05ad8f9c6f8b0879f51b05df4129ffd
Magnus Furnes Vadoy <magnus.f.vadoy@gmail.com>
2015-08-08 18:11:53 +0200

    moar

--- config.fish
+++ config.fish
@@ -1,3 +1,5 @@
 set -g WAHOO_PATH $HOME/.wahoo
 set -g WAHOO_CUSTOM $HOME/.dotfiles
 source $WAHOO_PATH/init.fish
+
+[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
