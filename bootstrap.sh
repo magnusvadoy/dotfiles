cc8ef61f7480c4c6a7cfe149d55bdfb38a0c48a5
Magnus Furnes Vadøy <mavad@fiskeridir.no>
2022-07-22 12:28:20 +0200

    added vim config to bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,10 @@ git submodule update
 
 touch ~/.env
 mkdir ~/.nvm
+mkdir ~/.vim
 
 ln -s $DIR/zshrc ~/.zshrc
 ln -s $DIR/tmux.conf ~/.tmux.conf
 ln -s $DIR/gitignore ~/.gitignore
 ln -s $DIR/nvm/default-packages ~/.nvm/default-packages
+ln -s $DIR/vim/vimrc ~/.vim/vimrc
