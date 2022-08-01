c7b58508d8c4b2ad38ef5c0e4e789656afec38a2
Magnus Furnes Vadøy <mavad@fiskeridir.no>
2022-08-01 15:57:10 +0200

    updated nvim config

--- install.sh
+++ install.sh
@@ -9,9 +9,14 @@ git submodule update
 touch ~/.env
 mkdir ~/.nvm
 mkdir ~/.vim
+mkdir -p ~/.config/nvim/lua
 
 ln -s $DIR/zshrc ~/.zshrc
 ln -s $DIR/tmux.conf ~/.tmux.conf
 ln -s $DIR/gitignore ~/.gitignore
 ln -s $DIR/nvm/default-packages ~/.nvm/default-packages
 ln -s $DIR/vim/vimrc ~/.vim/vimrc
+ln -s $DIR/nvim ~/.config/nvim
+
+# Install vim-plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
