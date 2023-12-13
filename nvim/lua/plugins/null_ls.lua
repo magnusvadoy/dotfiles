36378801d357e4bb685cc6eaee6206fe448c2a20
Magnus Furnes Vadøy <magnus.vadoy@tv2.no>
2023-12-13 19:51:48 +0100

    Removed autoformatting

--- nvim/lua/plugins/null_ls.lua
+++ nvim/lua/plugins/null_ls.lua
@@ -1,4 +1,4 @@
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 return {
   {
@@ -20,18 +20,18 @@ return {
           null_ls.builtins.formatting.prettierd,
           null_ls.builtins.formatting.stylua,
         },
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ async = false })
-              end,
-            })
-          end
-        end,
+        -- on_attach = function(client, bufnr)
+        --   if client.supports_method("textDocument/formatting") then
+        --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        --     vim.api.nvim_create_autocmd("BufWritePre", {
+        --       group = augroup,
+        --       buffer = bufnr,
+        --       callback = function()
+        --         vim.lsp.buf.format({ async = false })
+        --       end,
+        --     })
+        --   end
+        -- end,
       })
     end,
   },
