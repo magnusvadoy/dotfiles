0b1a0749ca5d7d1b2d08949c892464851ecdfd2c
Magnus Furnes Vadøy <magnus.vadoy@tv2.no>
2023-12-21 10:24:40 +0100

    Updated wezterm tab bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,15 @@
-local w = require("wezterm")
-local act = w.action
-local c = {}
+---@diagnostic disable: unused-local
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = {}
 
-if w.config_builder then
-  c = w.config_builder()
+if wezterm.config_builder then
+  config = wezterm.config_builder()
 end
 
 local function get_appearance()
-  if w.gui then
-    return w.gui.get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
   end
   return "Dark"
 end
@@ -42,7 +43,7 @@ local function split_nav(resize_or_move, key)
   return {
     key = key,
     mods = resize_or_move == "resize" and "META" or "CTRL",
-    action = w.action_callback(function(win, pane)
+    action = wezterm.action_callback(function(win, pane)
       if is_vim(pane) then
         -- pass the keys through to vim/nvim
         win:perform_action({
@@ -59,23 +60,99 @@ local function split_nav(resize_or_move, key)
   }
 end
 
+local process_icons = {
+  ['docker'] = wezterm.nerdfonts.linux_docker,
+  ['docker-compose'] = wezterm.nerdfonts.linux_docker,
+  ['kubectl'] = wezterm.nerdfonts.linux_docker,
+  ['psql'] = '󱤢',
+  ['usql'] = '󱤢',
+  ['nvim'] = wezterm.nerdfonts.custom_vim,
+  ['make'] = wezterm.nerdfonts.seti_makefile,
+  ['vim'] = wezterm.nerdfonts.dev_vim,
+  ['node'] = wezterm.nerdfonts.mdi_hexagon,
+  ['go'] = wezterm.nerdfonts.seti_go,
+  ['zsh'] = wezterm.nerdfonts.dev_terminal,
+  ['bash'] = wezterm.nerdfonts.cod_terminal_bash,
+  ['htop'] = wezterm.nerdfonts.mdi_chart_donut_variant,
+  ['cargo'] = wezterm.nerdfonts.dev_rust,
+  ['sudo'] = wezterm.nerdfonts.fa_hashtag,
+  ['git'] = wezterm.nerdfonts.dev_git,
+  ['lazygit'] = wezterm.nerdfonts.dev_git,
+  ['lua'] = wezterm.nerdfonts.seti_lua,
+  ['wget'] = wezterm.nerdfonts.mdi_arrow_down_box,
+  ['curl'] = wezterm.nerdfonts.mdi_flattr,
+  ['gh'] = wezterm.nerdfonts.dev_github_badge,
+  ['ruby'] = wezterm.nerdfonts.cod_ruby,
+}
+
 local function get_current_working_dir(tab)
-  return string.gsub(tab.active_pane.current_working_dir, "(.*[/\\])(.*)", "%2")
+  local current_dir = tab.active_pane.current_working_dir or ''
+  local HOME_DIR = string.format('file://%s', os.getenv('HOME'))
+
+  return current_dir == HOME_DIR and '.' or string.gsub(current_dir, '(.*[/\\])(.*)', '%2')
 end
 
--- set tab title to current working directory
-w.on("format-tab-title", function(tab)
-  local title = string.format(" %s %s ", "❯", get_current_working_dir(tab))
+local function get_process(tab)
+  if not tab.active_pane or tab.active_pane.foreground_process_name == '' then
+    return '[?]'
+  end
+
+  local process_name = string.gsub(tab.active_pane.foreground_process_name, '(.*[/\\])(.*)', '%2')
+  if string.find(process_name, 'kubectl') then
+    process_name = 'kubectl'
+  end
+
+  return process_icons[process_name] or string.format('[%s]', process_name)
+end
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, conf, hover, max_width)
+  local has_unseen_output = false
+  if not tab.is_active then
+    for _, pane in ipairs(tab.panes) do
+      if pane.has_unseen_output then
+        has_unseen_output = true
+        break
+      end
+    end
+  end
+
+  local cwd = wezterm.format({
+    { Attribute = { Intensity = 'Bold' } },
+    { Text = get_current_working_dir(tab) },
+  })
+
+  local title = string.format(' %s %s ', get_process(tab), cwd)
+
+  if has_unseen_output then
+    return {
+      { Foreground = { Color = '#28719c' } },
+      { Text = title },
+    }
+  end
+
   return {
     { Text = title },
   }
 end)
 
-local color_scheme = scheme_for_appearance(get_appearance())
-local colors = w.get_builtin_color_schemes()[color_scheme]
+-- https://wezfurlong.org/wezterm/config/lua/window-events/update-right-status.html
+wezterm.on('update-right-status', function(window)
+  if not window:get_dimensions().is_full_screen then
+    window:set_right_status('')
+    return
+  end
 
-c.color_scheme = color_scheme
-c.font = w.font_with_fallback({
+  window:set_right_status(wezterm.format({
+    { Foreground = { Color = '#808080' } },
+    { Text = wezterm.strftime(' %R ') },
+  }))
+end)
+
+local color_scheme = scheme_for_appearance(get_appearance())
+local colors = wezterm.get_builtin_color_schemes()[color_scheme]
+
+config.color_scheme = color_scheme
+config.font = wezterm.font_with_fallback({
   {
     family = "JetBrainsMono Nerd Font",
     harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
@@ -83,25 +160,25 @@ c.font = w.font_with_fallback({
   },
   "Apple Color Emoji",
 })
-c.font_size = 15
-c.window_padding = {
+config.font_size = 15
+config.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
 }
-c.window_background_opacity = 1.0
-c.use_fancy_tab_bar = false
-c.hide_tab_bar_if_only_one_tab = true
-c.tab_bar_at_bottom = true
-c.tab_max_width = 32
-c.colors = {
+config.window_background_opacity = 1.0
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = false
+config.tab_max_width = 32
+config.colors = {
   tab_bar = {
     background = colors.background,
   },
 }
 
-c.keys = {
+config.keys = {
   {
     key = "-",
     mods = "ALT",
@@ -131,4 +208,4 @@ c.keys = {
   split_nav("resize", "l"),
 }
 
-return c
+return config
