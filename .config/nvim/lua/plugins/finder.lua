0e2191b29b8fe22daecedd69ee0f879c3db1991f
Magnus Furnes Vadøy <magnus.vadoy@tv2.no>
2024-01-10 16:07:50 +0100

    moar

--- .config/nvim/lua/plugins/finder.lua
+++ .config/nvim/lua/plugins/finder.lua
@@ -1,85 +1,106 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      { "nvim-telescope/telescope-fzf-native.nvim",     build = "make" },
-      { "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
-      { "folke/todo-comments.nvim",                     dependencies = { "nvim-lua/plenary.nvim" }, opts = {} },
-      { "rmagatti/auto-session" },
-      { "nvim-lua/plenary.nvim" },
-    },
-    config = function()
-      local telescope = require("telescope")
-      local utils = require("telescope.utils")
-      local builtin = require("telescope.builtin")
-      local themes = require("telescope.themes")
-      local lga_actions = require("telescope-live-grep-args.actions")
-      local session_lens = require("auto-session.session-lens")
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			{ "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
+			{
+				"folke/todo-comments.nvim",
+				dependencies = { "nvim-lua/plenary.nvim" },
+				config = function()
+					local todo_comments = require("todo-comments")
+					todo_comments.setup({})
 
-      local map = function(mode, keys, func, desc)
-        vim.keymap.set(mode, keys, func, { desc = "Telescope: " .. desc })
-      end
+					vim.keymap.set("n", "]t", function()
+						todo_comments.jump_next()
+					end, { desc = "Next todo comment" })
 
-      map("n", "<leader>/", function()
-        builtin.current_buffer_fuzzy_find()
-      end, "Search current buffer")
-      map("n", "<leader>ff", function()
-        local _, ret, _ = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" })
-        if ret == 0 then
-          builtin.git_files({ show_untracked = true })
-        else
-          builtin.find_files()
-        end
-      end, "Find file")
-      map("n", "<leader>?", builtin.oldfiles, "Recently opened files")
-      map("n", "<leader><space>", function()
-        builtin.buffers({
-          ignore_current_buffer = true,
-          sort_mru = true,
-        })
-      end, "Find buffer")
-      map("n", "<leader>fc", builtin.command_history, "Find command history")
-      map("n", "<leader>fw", builtin.grep_string, "Find current word")
-      map("n", "<leader>fg", telescope.extensions.live_grep_args.live_grep_args, "Find grep")
-      map("n", "<leader>fr", builtin.resume, "Find resume")
-      map("n", "<leader>fd", builtin.diagnostics, "Find diagnostics")
-      map("n", "<leader>fh", builtin.help_tags, "Find help")
-      map("n", "<leader>fs", session_lens.search_session, "Find session")
-      map("n", "<leader>ga", builtin.git_status, "Status")
-      map("n", "<leader>gb", builtin.git_branches, "Branches")
-      map("n", "<leader>gc", builtin.git_commits, "Commits")
-      map("n", "<leader>ft", "<CMD>TodoTelescope<CR>", "Find todo")
+					vim.keymap.set("n", "[t", function()
+						todo_comments.jump_prev()
+					end, { desc = "Previous todo comment" })
+				end,
+			},
+			{ "rmagatti/auto-session" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		config = function()
+			local telescope = require("telescope")
+			local utils = require("telescope.utils")
+			local builtin = require("telescope.builtin")
+			local themes = require("telescope.themes")
+			local lga_actions = require("telescope-live-grep-args.actions")
+			local session_lens = require("auto-session.session-lens")
 
-      telescope.setup({
-        defaults = themes.get_ivy({
-          file_ignore_patterns = {
-            "%.DS_Store",
-            "%.git/",
-            "dist/",
-            "node_modules/",
-          },
-        }),
-        extensions = {
-          fzf = {
-            fuzzy = true,             -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-          },
-          live_grep_args = {
-            mappings = {
-              i = {
-                ["<C-k>"] = lga_actions.quote_prompt(),
-              },
-            },
-          },
-        },
-      })
+			local map = function(mode, keys, func, desc)
+				vim.keymap.set(mode, keys, func, { desc = "Telescope: " .. desc })
+			end
 
-      telescope.load_extension("fzf")
-      telescope.load_extension("live_grep_args")
-      telescope.load_extension("todo-comments")
-    end,
-  },
+			map("n", "<leader>/", function()
+				builtin.current_buffer_fuzzy_find()
+			end, "Search current buffer")
+			map("n", "<leader>ff", function()
+				local _, ret, _ = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" })
+				if ret == 0 then
+					builtin.git_files({ show_untracked = true })
+				else
+					builtin.find_files()
+				end
+			end, "Find file")
+			map("n", "<leader>?", builtin.oldfiles, "Recently opened files")
+			map("n", "<leader><space>", function()
+				builtin.buffers({
+					ignore_current_buffer = true,
+					sort_mru = true,
+				})
+			end, "Find buffer")
+			map("n", "<leader>fc", builtin.command_history, "Find command history")
+			map("n", "<leader>fw", builtin.grep_string, "Find current word")
+			map("n", "<leader>fg", telescope.extensions.live_grep_args.live_grep_args, "Find grep")
+			map("n", "<leader>fr", builtin.resume, "Find resume")
+			map("n", "<leader>fd", builtin.diagnostics, "Find diagnostics")
+			map("n", "<leader>fh", builtin.help_tags, "Find help")
+
+			-- Git
+			map("n", "<leader>ga", builtin.git_status, "Git Status")
+			map("n", "<leader>gb", builtin.git_branches, "Git Branches")
+			map("n", "<leader>gc", builtin.git_commits, "Git Commits")
+
+			-- Session
+			map("n", "<leader>fs", session_lens.search_session, "Find session")
+
+			-- Todo Comments
+			map("n", "<leader>ft", "<CMD>TodoTelescope<CR>", "Find todo")
+
+			telescope.setup({
+				defaults = themes.get_ivy({
+					file_ignore_patterns = {
+						"%.DS_Store",
+						"%.git/",
+						"dist/",
+						"node_modules/",
+					},
+				}),
+				extensions = {
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					},
+					live_grep_args = {
+						mappings = {
+							i = {
+								["<C-k>"] = lga_actions.quote_prompt(),
+							},
+						},
+					},
+				},
+			})
+
+			telescope.load_extension("fzf")
+			telescope.load_extension("live_grep_args")
+			telescope.load_extension("todo-comments")
+		end,
+	},
 }
--- .config/nvim/lua/plugins/syntax.lua
+++ .config/nvim/lua/plugins/syntax.lua
@@ -1,93 +1,93 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdateSync" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      { "nvim-treesitter/nvim-treesitter-context", opts = { max_lines = 3 } },
-      {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        config = function()
-          ---@diagnostic disable-next-line: missing-fields
-          require("ts_context_commentstring").setup({})
-          vim.g.skip_ts_context_commentstring_module = true
-        end,
-      },
-      "windwp/nvim-ts-autotag",
-    },
-    opts = {
-      auto_install = false,
-      sync_install = false,
-      ignore_install = {},
-      ensure_installed = {
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
-        "python",
-        "lua",
-        "typescript",
-        "javascript",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "tsx",
-        "svelte",
-        "html",
-        "css",
-        "scss",
-        "bash",
-        "yaml",
-        "toml",
-        "markdown",
-        "regex",
-        "diff",
-        "jsonnet",
-        "proto",
-        "scala",
-        "vimdoc",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<CR>", -- normal mode
-          scope_incremental = "<CR>", -- visual mode
-          node_incremental = "<Tab>", -- visual mode
-          node_decremental = "<S-Tab>", -- visual mode
-        },
-      },
-      -- nvim-ts-autotag
-      autotag = { enable = true },
-      -- :help nvim-treesitter-textobjects-modules
-      textobjects = {
-        select = {
-          enable = true,
-          keymaps = {
-            ["ac"] = { query = "@class.outer", desc = "TS: all class" },
-            ["ic"] = { query = "@class.inner", desc = "TS: inner class" },
-            ["af"] = { query = "@function.outer", desc = "TS: all function" },
-            ["if"] = { query = "@function.inner", desc = "TS: inner function" },
-          },
-        },
-        move = {
-          enable = true,
-          goto_next_start = {
-            ["]c"] = { query = "@class.outer", desc = "TS: Next class start" },
-            ["]f"] = { query = "@function.outer", desc = "TS: Next function start" },
-          },
-          goto_previous_start = {
-            ["[c"] = { query = "@class.outer", desc = "TS: Prev class start" },
-            ["[f"] = { query = "@function.outer", desc = "TS: Prev function start" },
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		cmd = { "TSUpdateSync" },
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter-textobjects" },
+			{ "nvim-treesitter/nvim-treesitter-context", opts = { max_lines = 3 } },
+			{ "windwp/nvim-ts-autotag", },
+			{
+				"JoosepAlviste/nvim-ts-context-commentstring",
+				config = function()
+					---@diagnostic disable-next-line: missing-fields
+					require("ts_context_commentstring").setup({})
+					vim.g.skip_ts_context_commentstring_module = true
+				end,
+			},
+		},
+		opts = {
+			auto_install = true,
+			sync_install = false,
+			ignore_install = {},
+			ensure_installed = {
+				"go",
+				"gomod",
+				"gowork",
+				"gosum",
+				"python",
+				"lua",
+				"typescript",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"tsx",
+				"svelte",
+				"html",
+				"css",
+				"scss",
+				"bash",
+				"yaml",
+				"toml",
+				"markdown",
+				"regex",
+				"diff",
+				"jsonnet",
+				"proto",
+				"scala",
+				"vimdoc",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<CR>", -- normal mode
+					scope_incremental = "<CR>", -- visual mode
+					node_incremental = "<Tab>", -- visual mode
+					node_decremental = "<S-Tab>", -- visual mode
+				},
+			},
+			-- nvim-ts-autotag
+			autotag = { enable = true },
+			-- :help nvim-treesitter-textobjects-modules
+			textobjects = {
+				select = {
+					enable = true,
+					keymaps = {
+						["ac"] = { query = "@class.outer", desc = "TS: all class" },
+						["ic"] = { query = "@class.inner", desc = "TS: inner class" },
+						["af"] = { query = "@function.outer", desc = "TS: all function" },
+						["if"] = { query = "@function.inner", desc = "TS: inner function" },
+					},
+				},
+				move = {
+					enable = true,
+					goto_next_start = {
+						["]c"] = { query = "@class.outer", desc = "TS: Next class start" },
+						["]f"] = { query = "@function.outer", desc = "TS: Next function start" },
+					},
+					goto_previous_start = {
+						["[c"] = { query = "@class.outer", desc = "TS: Prev class start" },
+						["[f"] = { query = "@function.outer", desc = "TS: Prev function start" },
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
 }
