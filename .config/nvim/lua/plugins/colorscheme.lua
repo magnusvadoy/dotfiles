3756742972d35da8f01d09c5e417dd766ce4f12d
Magnus Furnes Vadøy <magnus.vadoy@tv2.no>
2024-01-11 15:26:26 +0100

    Fixed light colorscheme and nvim-tree

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,9 +5,9 @@ return {
       {
         "folke/tokyonight.nvim",
         opts = {
-          style = "night",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-          light_style = "day",    -- The theme is used when the background is set to light
-          transparent = false,    -- Enable this to disable setting the background color
+          style = "night",   -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+          light_style = "day", -- The theme is used when the background is set to light
+          transparent = false, -- Enable this to disable setting the background color
           terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
           styles = {
             -- Style to be applied to different syntax groups
@@ -18,7 +18,7 @@ return {
             variables = {},
             -- Background styles. Can be "dark", "transparent" or "normal"
             sidebars = "dark", -- style for sidebars, see below
-            floats = "dark",   -- style for floating windows
+            floats = "dark", -- style for floating windows
           },
           on_highlights = function(hl, c)
             local prompt = "#2d3149"
@@ -68,27 +68,27 @@ return {
         priority = 1000,
         opts = {
           flavour = "mocha", -- latte, frappe, macchiato, mocha
-          background = {     -- :h background
+          background = { -- :h background
             light = "latte",
             dark = "mocha",
           },
           transparent_background = false, -- disables setting the background color.
-          show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
-          term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+          show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+          term_colors = false,       -- sets terminal colors (e.g. `g:terminal_color_0`)
           dim_inactive = {
-            enabled = false,              -- dims the background color of inactive window
+            enabled = false,         -- dims the background color of inactive window
             shade = "dark",
-            percentage = 0.15,            -- percentage of the shade to apply to the inactive window
+            percentage = 0.15,       -- percentage of the shade to apply to the inactive window
           },
-          no_italic = true,               -- Force no italic
-          no_bold = false,                -- Force no bold
-          no_underline = false,           -- Force no underline
+          no_italic = true,          -- Force no italic
+          no_bold = false,           -- Force no bold
+          no_underline = false,      -- Force no underline
           color_overrides = {},
           custom_highlights = {},
           integrations = {
             cmp = true,
             gitsigns = true,
-            nvimtree = false,
+            nvimtree = true,
             treesitter = true,
             treesitter_context = true,
             notify = false,
@@ -108,12 +108,12 @@ return {
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.api.nvim_set_option("background", "dark")
         vim.cmd("colorscheme catppuccin")
+        vim.api.nvim_set_option("background", "dark")
       end,
       set_light_mode = function()
-        vim.api.nvim_set_option("background", "light")
         vim.cmd("colorscheme catppuccin")
+        vim.api.nvim_set_option("background", "light")
       end,
     },
   },
