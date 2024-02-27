d9eabafec06e767449272181bc2b28a98b5a4c0a
Magnus Vadøy <magnus.vadoy@tv2.no>
2024-02-27 15:17:54 +0100

    More

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,147 +1,51 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 local function get_appearance()
-	if wezterm.gui then
-		return wezterm.gui.get_appearance()
-	end
-	return "Dark"
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return "Dark"
 end
 
 local function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Mocha"
-	else
-		return "Catppuccin Latte"
-	end
+  if appearance:find("Dark") then
+    return "Catppuccin Mocha"
+  else
+    return "Catppuccin Latte"
+  end
 end
 
-local process_icons = {
-	["docker"] = wezterm.nerdfonts.linux_docker,
-	["docker-compose"] = wezterm.nerdfonts.linux_docker,
-	["kubectl"] = wezterm.nerdfonts.md_kubernetes,
-	["k9s"] = wezterm.nerdfonts.md_kubernetes,
-	["psql"] = wezterm.nerdfonts.dev_database,
-	["usql"] = wezterm.nerdfonts.dev_database,
-	["nvim"] = wezterm.nerdfonts.dev_vim,
-	["make"] = wezterm.nerdfonts.seti_makefile,
-	["vim"] = wezterm.nerdfonts.dev_vim,
-	["node"] = wezterm.nerdfonts.dev_nodejs_small,
-	["npm"] = wezterm.nerdfonts.dev_npm,
-	["go"] = wezterm.nerdfonts.seti_go,
-	["zsh"] = wezterm.nerdfonts.dev_terminal,
-	["bash"] = wezterm.nerdfonts.cod_terminal_bash,
-	["htop"] = wezterm.nerdfonts.mdi_chart_donut_variant,
-	["cargo"] = wezterm.nerdfonts.dev_rust,
-	["sudo"] = wezterm.nerdfonts.fa_hashtag,
-	["git"] = wezterm.nerdfonts.dev_git,
-	["lazygit"] = wezterm.nerdfonts.dev_git,
-	["lua"] = wezterm.nerdfonts.seti_lua,
-	["wget"] = wezterm.nerdfonts.mdi_arrow_down_box,
-	["curl"] = wezterm.nerdfonts.mdi_flattr,
-	["gh"] = wezterm.nerdfonts.dev_github_badge,
-	["ruby"] = wezterm.nerdfonts.cod_ruby,
-	["adb"] = wezterm.nerdfonts.dev_android,
-	["python"] = wezterm.nerdfonts.cod_python,
-}
-
-local function get_current_working_dir(tab)
-	local current_dir = tab.active_pane.current_working_dir or ""
-	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
-
-	return current_dir == HOME_DIR and "." or string.gsub(current_dir, "(.*[/\\])(.*)", "%2")
-end
-
-local function get_process(tab)
-	if not tab.active_pane or tab.active_pane.foreground_process_name == "" then
-		return "[?]"
-	end
-
-	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
-	if string.find(process_name, "kubectl") then
-		process_name = "kubectl"
-	end
-
-	return process_icons[process_name] or string.format("[%s]", process_name)
-end
-
----@diagnostic disable-next-line: unused-local
-wezterm.on("format-tab-title", function(tab, tabs, panes, conf, hover, max_width)
-	local has_unseen_output = false
-	if not tab.is_active then
-		for _, pane in ipairs(tab.panes) do
-			if pane.has_unseen_output then
-				has_unseen_output = true
-				break
-			end
-		end
-	end
-
-	local cwd = wezterm.format({
-		{ Attribute = { Intensity = "Bold" } },
-		{ Text = get_current_working_dir(tab) },
-	})
-
-	local title = string.format(" %s %s ", get_process(tab), cwd)
-
-	if has_unseen_output then
-		return {
-			{ Foreground = { Color = "#28719c" } },
-			{ Text = title },
-		}
-	end
-
-	return {
-		{ Text = title },
-	}
-end)
-
--- https://wezfurlong.org/wezterm/config/lua/window-events/update-right-status.html
 wezterm.on("update-right-status", function(window)
-	if not window:get_dimensions().is_full_screen then
-		window:set_right_status("")
-		return
-	end
+  if not window:get_dimensions().is_full_screen then
+    window:set_right_status("")
+    return
+  end
 
-	window:set_right_status(wezterm.format({
-		{ Text = wezterm.strftime("%d-%m-%Y %H:%M:%S") },
-	}))
+  window:set_right_status(wezterm.format({
+    { Text = wezterm.strftime("%d-%m-%Y %H:%M:%S") },
+  }))
 end)
 
 config.color_scheme = scheme_for_appearance(get_appearance())
 config.font = wezterm.font("JetBrainsMono Nerd Font", {
-	weight = 500,
+  weight = 500,
 })
--- config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- disable ligatures
-config.font_size = 15
+config.font_size = 16
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 config.window_background_opacity = 1.0
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = false
 config.tab_max_width = 32
-
-config.keys = {
-	{
-		key = "-",
-		mods = "ALT",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "=",
-		mods = "ALT",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-}
 
 return config
