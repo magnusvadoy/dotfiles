a2d31b830fe85775ea2bef93b592721c5e7ce8e3
Magnus Vadøy <magnus.vadoy@tv2.no>
2024-02-28 08:30:30 +0100

    disabled focus for some filetypes

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -1,5 +1,33 @@
+---@diagnostic disable: inject-field
 -- ========================================================================== --
 -- ==                               AUTOCOMMANDS                           == --
 -- ========================================================================== --
 
--- local group = vim.api.nvim_create_augroup("user_cmds", { clear = true })
+local ignore_filetypes = { "nvim-tree", "neo-tree" }
+local ignore_buftypes = { "nofile", "prompt", "popup" }
+
+local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+
+vim.api.nvim_create_autocmd("WinEnter", {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+      vim.w.focus_disable = true
+    else
+      vim.w.focus_disable = false
+    end
+  end,
+  desc = "Disable focus autoresize for BufType",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+      vim.b.focus_disable = true
+    else
+      vim.b.focus_disable = false
+    end
+  end,
+  desc = "Disable focus autoresize for FileType",
+})
