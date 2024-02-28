140e2a8374986b1df14443401a97f1dbc54843fb
Magnus Vadøy <magnus.vadoy@tv2.no>
2024-02-28 15:59:49 +0100

    Fixed git conflict keybinding

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,7 +4,14 @@ return {
 		"akinsho/git-conflict.nvim",
 		version = "*",
 		opts = {
-			default_mappings = true, -- disable buffer local mapping created by this plugin
+			default_mappings = {
+				ours = "o",
+				theirs = "t",
+				none = "0",
+				both = "b",
+				next = "]x",
+				prev = "[x",
+			},
 			default_commands = true, -- disable commands created by this plugin
 			disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
 			list_opener = "copen", -- command or function to open the conflicts list
