81fa130cdaaecc1cbb66e8f771cba894b0589df0
Magnus Vadøy <magnus.vadoy@tv2.no>
2024-05-14 07:57:18 +0200

    Fixed tmux conf

--- .config/nvim/lua/plugins/yaml-companion.lua
+++ .config/nvim/lua/plugins/yaml-companion.lua
@@ -6,9 +6,9 @@ return {
 			{ "nvim-lua/plenary.nvim" },
 			{ "nvim-telescope/telescope.nvim" },
 		},
+		ft = { "yaml" },
 		config = function()
 			require("telescope").load_extension("yaml_schema")
-
 			vim.keymap.set("n", "<leader>fs", "<cmd>Telescope yaml_schema<cr>", { desc = "Schema" })
 		end,
 	},
