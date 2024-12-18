24aa5043bd7003c9d8e8db5a96dd91846424eb34
Magnus Vadøy <magnus.vadoy@tv2.no>
2024-12-18 13:20:47 +0100

    Updated gitignore, fixed text objects, fixed fish yazi cmd

--- .config/fish/functions/yazi.fish
+++
@@ -1,9 +0,0 @@
-# change the current working directory when exiting Yazi
-function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
-end
