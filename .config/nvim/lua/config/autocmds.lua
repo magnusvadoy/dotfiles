7baae864cb5e842307e4e97ea939d74c775fbb43
Magnus Vadøy <magnus.vadoy@tv2.no>
2025-02-04 10:49:40 +0100

    Added commentstring for proto files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,10 +7,18 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- Disable autoformat for yaml files
+-- Disable autoformat for yaml and proto files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "yaml", "proto" },
   callback = function()
     vim.b.autoformat = false
+  end,
+})
+
+-- Set commentstring for proto files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "proto",
+  callback = function()
+    vim.bo.commentstring = "// %s"
   end,
 })
