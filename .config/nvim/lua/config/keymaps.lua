20efb5a86347c50a3900cb819e1497e073574938
Magnus Vadøy <magnus.vadoy@tv2.no>
2025-02-06 09:54:48 +0100

    Swapped mini files for Oil, updated plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remove default keymaps that are never used
+vim.keymap.del("n", "<leader>`")
+vim.keymap.del("n", "<leader>ft")
+vim.keymap.del("n", "<leader>fT")
---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,25 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+  keys = {
+    {
+      "<leader>fm",
+      function()
+        require("oil").toggle_float()
+      end,
+      desc = "Open Oil (Current Directory)",
+    },
+    {
+      "<leader>fM",
+      function()
+        require("oil").toggle_float(vim.uv.cwd())
+      end,
+      desc = "Open Oil (cwd)",
+    },
+  },
+}
