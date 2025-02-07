aa30a1e741fd000e976fc733a33614a6a5adf338
Magnus Vadøy <magnus.vadoy@tv2.no>
2025-02-07 14:56:23 +0100

    Added rg source and colorful menus

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -8,7 +8,7 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 -- Disable autoformat for yaml and proto files
-vim.api.nvim_create_autocmd({ "FileType" }, {
+vim.api.nvim_create_autocmd("FileType", {
   pattern = { "yaml", "proto" },
   callback = function()
     vim.b.autoformat = false
@@ -20,5 +20,26 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "proto",
   callback = function()
     vim.bo.commentstring = "// %s"
+  end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+  pattern = {
+    "Oil",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
   end,
 })
--- .config/nvim/lua/plugins/lazyvim.lua
+++ .config/nvim/lua/plugins/lazyvim.lua
@@ -57,7 +57,7 @@ return {
     },
   },
   {
-    "snacks.nvim",
+    "folke/snacks.nvim",
     keys = {
       { "<leader>.", false },
       { "<leader>S", false },
@@ -68,6 +68,47 @@ return {
           Snacks.notifier.show_history()
         end,
         desc = "Notification History",
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "mikavilpas/blink-ripgrep.nvim",
+      "xzbdmw/colorful-menu.nvim",
+    },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      sources = {
+        default = {
+          "ripgrep",
+        },
+        providers = {
+          ripgrep = {
+            module = "blink-ripgrep",
+            name = "Ripgrep",
+          },
+        },
+      },
+      completion = {
+        menu = {
+          draw = {
+            -- We don't need label_description now because label and label_description are already
+            -- combined together in label by colorful-menu.nvim.
+            columns = { { "kind_icon" }, { "label", gap = 1 } },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+            },
+          },
+        },
       },
     },
   },
