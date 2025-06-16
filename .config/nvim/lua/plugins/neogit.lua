7b00864eaf2bab31dc3990ce360519d7f5973dbe
Magnus Vadøy <magnus.vadoy@tv2.no>
2025-06-16 18:38:14 +0200

    Revert "Updated plugins"
    This reverts commit 035a65d1bf895fe85c92cc58c6a338497277061d.

---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "TimUntersberger/neogit",
+    enabled = false,
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<CR>", mode = { "n" }, desc = "Open Neogit" },
+    },
+  },
+}
